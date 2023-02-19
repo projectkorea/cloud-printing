9aad9c81c9dd337bb0dc234e4dc612632a09281e
Leo <prodopark15@gmail.com>
2023-02-19 13:42:25 +0900

    feat: apply libaray

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
-import express from 'express';
-import {oauth} from './routes/oauth.js';
-import {sfapi} from './routes/sfapi.js';
+import express from 'express'
+import { AuthorizationCode } from 'simple-oauth2'
+import cors from 'cors'
 
-const app = express();
+const app = express()
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const KAKAO = {
+    CLIENT_ID: 'ba0978cda441cd7c19ec044f0be74fbe',
+    // CLIENT_SECRET: 'rXxWfn0zwmuldPPsHERHC8Lt2xACl9yU',
+}
 
-app.use('/oauth', oauth);
-app.use('/sfapi', sfapi);
+const config = {
+    client: {
+        id: KAKAO.CLIENT_ID,
+        secret: KAKAO.CLIENT_SECRET,
+    },
+    auth: {
+        authorizePath: '/oauth/authorize',
+        tokenHost: 'https://kauth.kakao.com',
+        tokenPath: '/oauth/token',
+    },
+    options: {
+        bodyFormat: 'form',
+    },
+}
+
+console.log(config.client.id, config.client.secret)
+app.get('/oauth', (req, res) => {
+    const client = new AuthorizationCode(config)
+    const authorizationUri = client.authorizeURL({
+        redirect_uri: 'http://localhost:3001/callback',
+    })
+    console.log('authorizationUri', authorizationUri)
+    res.redirect(authorizationUri)
+})
+
+app.get('/callback', async (req, res) => {
+    const client = new AuthorizationCode(config)
+    const tokenParams = {
+        code: req.query.code,
+        redirect_uri: 'http://localhost:3001/callback',
+    }
+    const httpOptions = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+    }
+    try {
+        const accessToken = await client.getToken(tokenParams, httpOptions)
+        console.log('✅ Access Token Success', accessToken)
+        res.send('Access token: ' + accessToken.token.access_token)
+    } catch (error) {
+        console.log('❌ Access Token Error', error)
+        res.send('Error: ' + error)
+    }
+})
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-});
+    res.sendFile('index.html', { root: 'public' })
+})
 
-app.listen('3001', () => console.log(`✅SERVER ON: http://localhost:3001`));
+app.listen(3001, () => {
+    console.log(`🌈Server started on port 3001`)
+})
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,39 +1,48 @@
-import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode} from "simple-oauth2"
+import express from 'express'
+import { AuthorizationCode } from 'simple-oauth2'
+
+const app = express()
+
+const KAKAO = {
+    CLIENT_ID: 'ae74ca8408e21c2e29915ceee47739fc',
+    CLIENT_SECRET: 'kwhQ40xHQOagUsoVZYUp5KIGC3FjdCm8',
+}
 
 const config = {
     client: {
-        id: '<client-id>',
-        secret: '<client-secret>'
+        id: KAKAO.CLIENT_ID,
+        secret: KAKAO.CLIENT_SECRET,
     },
     auth: {
-        tokenHost: 'https://api.oauth.com'
-    }
-};
-
-async function run() {
-    const client = new AuthorizationCode(config);
-
-    const authorizationUri = client.authorizeURL({
-        redirect_uri: 'http://localhost:3000/callback',
-        scope: '<scope>',
-        state: '<state>'
-    });
-    // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
-    res.redirect(authorizationUri);
-    const tokenParams = {
-        code: '<code>',
-        redirect_uri: 'http://localhost:3000/callback',
-        scope: '<scope>',
-    };
-    try {
-        const accessToken = await client.getToken(tokenParams);
-    } catch (error) {
-        console.log('Access Token Error', error.message);
-    }
+        tokenHost: 'https://kauth.kakao.com',
+    },
 }
 
-run();
+async function run(req, res) {
+    const client = new AuthorizationCode(config)
+    const authorizationUri = client.authorizeURL({
+        redirect_uri: 'http://localhost:3001/callback',
+    })
 
-export function oauth(){
+    console.log('authorizationUri', authorizationUri)
+    res.redirect(authorizationUri)
 
+    // const tokenParams = {
+    //     code: '<code>',
+    //     redirect_uri: 'http://localhost:3000/callback',
+    // };
+
+    // try {
+    //     const accessToken = await client.getToken(tokenParams);
+    //     console.log('✅ Access Token Success', accessToken)
+    // } catch (error) {
+    //     console.log('❌ Access Token Error', error.message);
+    // }
+}
+
+run()
+
+export function oauth(req, res) {
+    console.log(req, res)
+    run(req, res)
 }
