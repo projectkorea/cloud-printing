ef81fa50ae0675228cfc9601dd0b9baa8a4579a6
Leo <prodopark15@gmail.com>
2023-02-21 23:44:10 +0900

    feat: implement oauth for ezeep

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import fetch from 'node-fetch'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import { oAuthCallback } from './routes/oauth.js'
 
 dotenv.config()
 
@@ -11,37 +12,14 @@ const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 
-const CONFIG = {
-    url: 'https://kauth.kakao.com/oauth/token',
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: 'https://wiseprint.cloud/callback',
-    code: null,
-    userInfoUrl: 'https://kapi.kakao.com/v2/user/me',
-}
-
-// app.use('/oauth', (req, res) => {
-//     console.log(`app.get '/oauth'`)
-//     const authorizationURI = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CONFIG.clientID}&redirect_uri=${CONFIG.redirectUri}`
-//     res.redirect(authorizationURI)
-// })
-
-// app.use('/callback', async (req, res) => {
-//     console.log(`app.get '/callback'`)
-//     CONFIG.code = req.query.code
-//     const token = await getAccessToken(CONFIG)
-//     console.log('Success to get token', token)
-//     const userInfo = await getUserInfo(CONFIG.userInfoUrl, token.access_token)
-
-//     res.send(userInfo)
-// })
-
-// app.use((req, res) => {
-//     res.status(404).redirect('/')
-// })
+app.use('/oauth/ezeep', oAuthCallback)
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' })
+})
+
+app.use((req, res) => {
+    res.status(404).redirect('/')
 })
 
 app.listen(3001, () => {
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,21 +1,18 @@
-export const getAccessToken = async (options) => {
-    try {
-        return await fetch(options.url, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-            },
-            body: qs.stringify({
-                grant_type: 'authorization_code',
-                client_id: options.clientID,
-                client_secret: options.clientSecret,
-                redirectUri: options.redirectUri,
-                code: options.code,
-            }),
-        }).then((res) => res.json())
-    } catch (e) {
-        logger.info('error', e)
-    }
+const CONFIG = {
+    url: 'https://account.ezeep.com/oauth/access_token',
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'https://wiseprint.cloud/callback',
+    code: null,
+}
+
+export const oAuthCallback = async (req, res) => {
+    updateConfigCode(req.query.code)
+    const token = await getAccessToken(CONFIG)
+    console.log('Success to get token', token)
+    // const userInfo = await getUserInfo(CONFIG.userInfoUrl, token.access_token)
+
+    res.send(userInfo)
 }
 
 export const getUserInfo = async (url, access_token) => {
@@ -30,4 +27,29 @@ export const getUserInfo = async (url, access_token) => {
     } catch (e) {
         logger.info('error', e)
     }
+}
+
+const getAccessToken = async (options) => {
+    try {
+        const auth = Buffer.from(`${options.clientID}:${options.clientSecret}`).toString('base64')
+        return await fetch(options.url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+                Authorization: `Basic ${auth}`,
+            },
+            body: qs.stringify({
+                grant_type: 'authorization_code',
+                scope: 'printing',
+                redirectUri: options.redirectUri,
+                code: options.code,
+            }),
+        }).then((res) => res.json())
+    } catch (e) {
+        logger.info('error', e)
+    }
+}
+
+function updateConfigCode(code) {
+    CONFIG.code = code
 }
