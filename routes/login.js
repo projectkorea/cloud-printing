dac75b0ec5828e331ba7f1a23eb0a170b901982f
Leo <prodopark15@gmail.com>
2023-03-05 17:53:09 +0900

    feat: github 로그인

---
+++ routes/login.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const githubLoginPage = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GITHUB_CLIENT,
+        scope: "read:user user:email",
+        allow_signup: true,
+    };
+    const params = new URLSearchParams(config).toString()
+    const finalUrl = `${baseUrl}?${params}`
+
+  return res.redirect(finalUrl)
+};
