ec1a217af4c47424c42cfac476a11c7780ed8cfb
Leo <prodopark15@gmail.com>
2023-03-08 13:19:25 +0900

    feat: kakao

--- routes/kakao.js
+++ routes/kakao.js
@@ -14,7 +14,6 @@ const KAKAO = {
 
 export const kakaoOAuthCallback = async (req, res) => {
     const token = await getKakaoAccessToken(req.query.code)
-    res.redirect('/')
     res.send(token)
 }
 
