e484becd077bf5dde9f3091836ab60d7c6182f24
Leo <prodopark15@gmail.com>
2023-04-01 21:09:26 +0900

    feat: printUploadedFiles

--- routes/ezeep.js
+++ routes/ezeep.js
@@ -138,14 +138,30 @@ export const fileUpload = async (req, res) => {
     }
 }
 
+function getTempValue() {
+    const BrotherDCP = '8d5f5242-a188-4965-a7cd-9bb13f9a3642'
+    const MicrosoftPrint = '9cd91d4f-1825-431c-a342-31b418616249'
+    const SEC842519C14383 = '618d7c34-396d-4694-b614-0c1862f8c9c6'
+
+    const fileid = req.session.fileIds.one
+    const printerid = BrotherDCP
+    const type = 'pdf'
+
+    return { fileid, printerid, type }
+}
+
 export const printUploadedFile = async (req, res) => {
     try {
-        const response = await fetch(`${CONFIG.baseURL}/sfapi/PrepareUpload/`, {
-            method: 'GET',
+        const { fileid, printerid, type } = getTempValue()
+        const response = await fetch(`${CONFIG.baseURL}/sfapi/Print/`, {
+            method: 'POST',
             headers: {
                 Authorization: `Bearer ${req.session.accessToken}`,
+                'Content-Type': 'application/json',
             },
+            body: JSON.stringify({ fileid, printerid, type }),
         })
+
         const data = await response.json()
         res.send(data)
     } catch (error) {
